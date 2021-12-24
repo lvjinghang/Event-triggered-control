dc9edb60391954f47d46ef8abf8e0e639e39b810
YangGu7023 <95921593+YangGu7023@users.noreply.github.com>
2021-12-24 15:28:06 +0800

    Update dynamic_relative_ETM.m

--- dynamic_relative_ETM.m
+++ dynamic_relative_ETM.m
@@ -1,7 +1,6 @@
-
 tm=40;h=0.001;t=0:h:tm;
 n=1;  
-Design_K_P;
+% Design_K_P;
 G=[0 0 1.0;
    0 -1.0 0;
    -1.0 0 0];
@@ -12,31 +11,36 @@ rank(OBSER);
 List=[2,2,2,1,4,3,3,1,3,4,4,1,3,1,1,2,2,3,3,1,1,4,4,2,2,3,1,4,4,2,2,2,2,1,1,2,3,3,1,1,1,2,2,2,1,4,3,3,1,3,4,4,1,3,1,1,2,2,3,3,1,1,4,4,2,2,3,1,4,4,2,2,2,2,1,1,2,3,3,1,1,1,2,2,2,1,4,3,3,1,3,4,4,1,3,1,1,2,2,3,3,1,1,4,4,2,2,3,1,4,4,2,2,2,2,1,1,2,3,3,1,1,1];
 k=2;
 % threshold parameter
-m2=0.05;
-max_delta2=2.5;%max_phi=max_delta*m1
-min_delta2=1.0;%min_phi=min_delta*m1
+max_phi2=0.125;
+min_phi2=0.05;
 max_delta_r=0.12;
 min_delta_r=0.08;
-topm2=0.145;%topm1>=max_phi/(1-max_delta_r)
-vare=1/5;Count_2=0;r2=1;
-
+topm2=0.145;
+%other parameter
+vare=1/5;
+Count_2=0;%the flag of triggering times
+r2=1;
+Gamma=1;
+a=10;
+b=1;
 while n<=(tm/h)+1;
     if n==1
      %% Initialization
         x2(1,:)=[-4,3.5,3.0];
-        D_es12(1)=0.0;D_es22(1)=0.0;D_es32(1)=0.0;
-        d1_es2(1)=0;
+        D_es12(1)=0.0;D_es22(1)=0.0;D_es32(1)=0.0;%The estimate of D
+        d1_es2(1)=0;%The estimate of disturbance
         rho_es2(1,:)=[0.0 0.0 0.0];
         vd2(1,:)=[0.0 0.0 0.0];
         u2(1)=0;
-        d12(1)=0;intervals2(1)=1;
+        d12(1)=0;intervals2(1)=0;
         z2(1,:)=[0.35 0.35 0.35];
-        w2(1)=0;delta2(1)=1.0;
+        w2(1)=0;phi2(1)=1.0;
         A=A2;B=B2;C1=C12;C2=C22;P=P2;K=K2;L_o=L_o2;Bw=Bw2;F=F2;
         delta_r(1)=0.08;
     else
         t1=h*(n-1);
         t2=h*n;
+        sigma(n)=0.01*exp(-5*t1);
      %% Mode
         if mod(n,1000)==0
             flag=List(k);
@@ -52,12 +56,12 @@ while n<=(tm/h)+1;
         end
         k=k+1;
         end
-     %%  ADOB 
+     %%  ADOB Gamma=1;
         vd2(n,:)=h*(M*rho_es2(n-1,:)'+L_o*(A1*x2(n-1,:)'+B*w2(n-1)))'+vd2(n-1,:);
         rho_es2(n,:)=vd2(n-1,:)-(L_o*x2(n-1,:)')';
-        D_es12(n)=h*(-0.00*D_es12(n-1)+1*B'*P*x2(n-1,:)'*rho_es2(n-1,1))+D_es12(n-1);
-        D_es22(n)=h*(-0.00*D_es12(n-1)+1*B'*P*x2(n-1,:)'*rho_es2(n-1,2))+D_es22(n-1);
-        D_es32(n)=h*(-0.00*D_es12(n-1)+1*B'*P*x2(n-1,:)'*rho_es2(n-1,3))+D_es32(n-1);
+        D_es12(n)=h*(-Gamma*sigma(n)*D_es12(n-1)+Gamma*B'*P*x2(n-1,:)'*rho_es2(n-1,1))+D_es12(n-1);
+        D_es22(n)=h*(-Gamma*sigma(n)*D_es12(n-1)+Gamma*B'*P*x2(n-1,:)'*rho_es2(n-1,2))+D_es22(n-1);
+        D_es32(n)=h*(-Gamma*sigma(n)*D_es12(n-1)+Gamma*B'*P*x2(n-1,:)'*rho_es2(n-1,3))+D_es32(n-1);
         D_es(n-1,:)=[D_es12(n-1)  D_es22(n-1) D_es32(n-1)];
         d1_es2(n)=D_es(n-1,:)*rho_es2(n-1,:)';
      %% Disturance
@@ -73,12 +77,12 @@ while n<=(tm/h)+1;
         
      %% ET     
         e=w2(n)-u2(n-1);
-        delta2(n)=max_delta2-(max_delta2-min_delta2)*tanh((10*e*e)/(u2(n-1)*u2(n-1)));
-        if delta2(n)>=max_delta2
-            delta2(n)=max_delta2;
+        phi2(n)=max_phi2-(max_phi2-min_phi2)*tanh((a*e*e)/b*(u2(n-1)*u2(n-1)));
+        if phi2(n)>=max_phi2
+            phi2(n)=max_phi2;
         end
-        if delta2(n)<=min_delta2
-            delta2(n)=min_delta2;
+        if phi2(n)<=min_phi2
+            phi2(n)=min_phi2;
         end
         delta_r(n)=max_delta_r-(max_delta_r-min_delta_r)*tanh((1*e*e)/(u2(n-1)*u2(n-1)));
         if delta_r(n)>=max_delta_r
@@ -87,7 +91,7 @@ while n<=(tm/h)+1;
         if delta_r(n)<=min_delta_r
             delta_r(n)=min_delta_r;
         end
-        if norm(e,inf)>(delta_r(n)*norm(u2(n-1),inf)+delta2(n)*m2)
+        if norm(e,inf)>(delta_r(n)*norm(u2(n-1),inf)+phi2(n))
         u2(n)=w2(n);
         Count_2=Count_2+1;
         Lount_1(n)=1;
@@ -105,4 +109,3 @@ while n<=(tm/h)+1;
     end
     n=n+1;
 end
-
